7427cb2a117fdaf4950f961b083f1ef2d22a1e32
Omkar Pawar <pawar.omkar2001@gmail.com>
2021-10-05 11:47:50 +0530

    Password authentication, logout

--- scripts/display.js
+++ scripts/display.js
@@ -3,23 +3,35 @@ document.getElementById("submit").onclick = display;
 var user;
 function display() {
   var email = document.getElementById("email").value;
-  console.log(email);
+  var pass = document.getElementById("password").value;
 
-  db.collection("users")
+
+  db.collection("auth")
     .doc(email)
     .get()
     .then((querySnapshot) => {
+
       let data = querySnapshot.data();
-      // const user = data.Name;
-      user = data.Name;
+      if (pass == data.Password) {
+        user = data.Name;
         console.log(user);
         var url = "/dash.html?name=" + user;
         document.location.href = url;
+      }
+      else {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Password Mismatch. Kindly re-check your password",
+          });
+      }
+      
+
     })
     .catch((err) => {
       console.log(`Error: ${err}`);
     });
-  
+
   // return user;
 
   // window.location.href = "/dash.html";
