4e1fc002d768f6d86b1f84a5d78597a318326b41
Omkar Pawar <pawar.omkar2001@gmail.com>
2021-10-05 23:04:10 +0530

    cookies with bugs

--- scripts/display.js
+++ scripts/display.js
@@ -1,10 +1,41 @@
 import { db } from "./firebase_config.js";
 document.getElementById("submit").onclick = display;
 var user;
+var mobile;
+var address;
+var type;
+
+
+
+function setCookie(email) {
+  db.collection("users")
+    .doc(email)
+    .get()
+    .then((querySnapshot) => {
+      let data = querySnapshot.data();
+      console.log(data);
+      user = data.Name;
+      mobile = data.MobileNo;
+      address = data.Address;
+      type = data.Type;
+      // console.log(user, mobile, address);
+      document.cookie = "email = " + email;
+      document.cookie = "username = " + user;
+      document.cookie = "mobile = " + mobile;
+      document.cookie = "address = " + address;
+      document.cookie = "type =" + type;
+      console.log(document.cookie);
+    })
+    .catch((err) => {
+      console.log(`Error: ${err}`);
+    });
+  
+  
+}
 function display() {
   var email = document.getElementById("email").value;
   var pass = document.getElementById("password").value;
-
+  console.log(email, pass);
 
   db.collection("auth")
     .doc(email)
@@ -14,7 +45,7 @@ function display() {
       let data = querySnapshot.data();
       if (pass == data.Password) {
         user = data.Name;
-        console.log(user);
+        setCookie(email);
         var url = "/dash.html?name=" + user;
         document.location.href = url;
       }
