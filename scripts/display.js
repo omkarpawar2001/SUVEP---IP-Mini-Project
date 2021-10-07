d847a0680f1ede30dc13589892777e85ec0b20aa
Omkar Pawar <pawar.omkar2001@gmail.com>
2021-10-07 18:10:34 +0530

    cookies bugs fixed

--- scripts/display.js
+++ scripts/display.js
@@ -7,30 +7,33 @@ var type;
 
 
 
-function setCookie(email) {
-  db.collection("users")
-    .doc(email)
-    .get()
-    .then((querySnapshot) => {
-      let data = querySnapshot.data();
-      console.log(data);
-      user = data.Name;
-      mobile = data.MobileNo;
-      address = data.Address;
-      type = data.Type;
-      // console.log(user, mobile, address);
-      document.cookie = "email = " + email;
-      document.cookie = "username = " + user;
-      document.cookie = "mobile = " + mobile;
-      document.cookie = "address = " + address;
-      document.cookie = "type =" + type;
-      console.log(document.cookie);
-    })
-    .catch((err) => {
-      console.log(`Error: ${err}`);
-    });
-  
-  
+async function setCookie(email) {
+  console.log("Inside setcookie");
+  // db.collection("users")
+  //   .doc(email)
+  //   .get()
+  //   .then((querySnapshot) => {
+  //     let data = querySnapshot.data();
+  //     console.log(data);
+  //     user = data.Name;
+  //     mobile = data.MobileNo;
+  //     address = data.Address;
+  //     type = data.Type;
+  //     // console.log(user, mobile, address);
+  //     document.cookie = "email = " + email;
+  //     document.cookie = "username = " + user;
+  //     document.cookie = "mobile = " + mobile;
+  //     document.cookie = "address = " + address;
+  //     document.cookie = "type =" + type;
+  //     console.log(document.cookie);
+  //     return Promise.resolve(1);
+
+  //   })
+  //   .catch((err) => {
+  //     console.log(`Error: ${err}`);
+  //     return Promise.resolve(0);
+
+  //   });
 }
 function display() {
   var email = document.getElementById("email").value;
@@ -45,9 +48,38 @@ function display() {
       let data = querySnapshot.data();
       if (pass == data.Password) {
         user = data.Name;
-        setCookie(email);
-        var url = "/dash.html?name=" + user;
-        document.location.href = url;
+        // setCookie(email).then(() => {
+        //   setCookie(email);
+        //   console.log(document.cookie);
+        //   var url = "/dash.html?name=" + user;
+        //   document.location.href = url;
+        // });
+        db.collection("users")
+          .doc(email)
+          .get()
+          .then((querySnapshot) => {
+            let data = querySnapshot.data();
+            console.log(data);
+            user = data.Name;
+            mobile = data.MobileNo;
+            address = data.Address;
+            type = data.Type;
+            // console.log(user, mobile, address);
+            document.cookie = "email = " + email;
+            document.cookie = "username = " + user;
+            document.cookie = "mobile = " + mobile;
+            document.cookie = "address = " + address;
+            document.cookie = "type =" + type;
+            console.log(document.cookie);
+            var url = "/dash.html?name=" + user;
+            document.location.href = url;
+          })
+          .catch((err) => {
+            console.log(`Error: ${err}`);
+            return Promise.resolve(0);
+          });
+        
+        
       }
       else {
         Swal.fire({
