99675721254a5701e1df866ae496746d5ae57b6a
Omkar Pawar <pawar.omkar2001@gmail.com>
2021-10-08 19:21:53 +0530

    fixed profie page issues

---
+++ scripts/updateprofile.js
@@ -0,0 +1,58 @@
+import { db } from './firebase_config.js'
+import {setCookieLogin} from './cookiestore.js'
+
+document.getElementById('submit').onclick = insert;
+
+function insert() {
+  var name = document.getElementById("fullname").value;
+  var email = document.getElementById("email").value;
+  var mobile = document.getElementById("mobile").value;
+  var address = document.getElementById("address1").value;
+  var type = document.getElementById("type").value;
+  console.log(name);
+
+
+  const user_data = {
+    Name: name,
+    Email: email,
+    MobileNo: mobile,
+    Address: address,
+    Type: type,
+  };
+
+
+
+
+
+  const insert = db
+    .collection("users")
+    .doc(email)
+    .set(user_data)
+    .then(function () {
+      console.log("Added to the database");
+    });
+ 
+  insert.then(
+    () => {
+
+      Swal.fire(
+        'Congratulations!',
+        'Your profile has been updated succesfully..!',
+        'success'
+      ).then(
+        () => {
+          setCookieLogin(email,name,mobile,address,type);
+          location.reload();
+        }
+      )
+    },
+    (error) => {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+      });
+    }
+  );
+}
+
