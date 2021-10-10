264a46b7e895feb37a02c93f9ae1e4afe8790649
Omkar Pawar <pawar.omkar2001@gmail.com>
2021-10-10 12:59:28 +0530

    chng password

---
+++ scripts/chngpwd.js
@@ -0,0 +1,59 @@
+import { getCookieLogin, setCookieLogin } from "./cookiestore.js";
+import { storage, db } from "./firebase_config.js";
+document.getElementById("submit").onclick = display;
+var user;
+var mobile;
+var address;
+var type;
+
+function display() {
+    var email = getCookieLogin("email");
+    var name = getCookieLogin("username");
+  var current = document.getElementById("current").value;
+  var newpass = document.getElementById("new").value;
+  var confirmpass = document.getElementById("newconfirm").value;
+  console.log(current, newpass, confirmpass);
+
+  db.collection("auth")
+    .doc(email)
+    .get()
+    .then((querySnapshot) => {
+      let data = querySnapshot.data();
+      if (current == data.Password) {
+        const auth = {
+          Name: name,
+          Email: email,
+          Password: newpass,
+        };
+        db.collection("auth")
+          .doc(email)
+          .set(auth)
+          .then(function () {
+            Swal.fire(
+              "Congratulations!",
+              "Your password has been updated succesfully..!!",
+              "success"
+            );
+          })
+          .catch((err) => {
+            console.log(`Error: ${err}`);
+          });
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Current Password Mismatch. Kindly re-check your password",
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(`Error: ${err}`);
+    });
+
+  //   // return user;
+
+  //   // window.location.href = "/dash.html";
+  //   // document.getElementById("data").innerHTML = email;
+  //   // console.log(email);
+}
+export { display };
